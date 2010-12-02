71f932a065fd02f57b84e1b089bb1a1ad61155ce
Joseph Wecker <jwecker@JTVs-MacBook-Pro.local>
2010-12-02 12:58:16 -0800

    Turning all of the typedefs into classes to make them more managable and readable.

--- haxe/src/ETF.hx
+++ haxe/src/ETF.hx
@@ -2,7 +2,7 @@ import flash.utils.ByteArray;
 
 class ETF {
     static public function decode(dat :ByteArray) :Dynamic {
-        var identifier_byte = dat.readByte();
+        var identifier_byte = dat.readUnsignedByte();
         switch(identifier_byte) {
             // Term
             case 131: return(decode(dat));
@@ -20,29 +20,22 @@ class ETF {
                       return Std.parseFloat(float_string);
             // Atom (old)
             case 100: var len = dat.readUnsignedShort();
-                      return {atom: dat.readUTFBytes(len)};
+                      return new ErlAtom(dat.readUTFBytes(len));
             // Reference (old)
             case 101: var id = new Array<UInt>();
                       var node = decode(dat);
                       id.push(dat.readUnsignedInt());
-                      var ref :ErlRef = {
-                          node:     node,
-                          id:       id,
-                          creation: dat.readUnsignedByte()};
-                      return ref;
+                      return new ErlRef(node, id, dat.readUnsignedByte());
             // Port
-            case 102: var port :ErlPort = {
-                          node:     decode(dat),
-                          id:       dat.readUnsignedInt(),
-                          creation: dat.readUnsignedByte()};
-                      return port;
+            case 102: return new ErlPort(
+                              decode(dat),             // Node
+                              dat.readUnsignedInt(),   // ID
+                              dat.readUnsignedByte()); // Creation
             // PID
-            case 103: var pid :ErlPID = {
-                          node:     decode(dat),
-                          id:       dat.readUnsignedInt(),
-                          serial:   dat.readUnsignedInt(),
-                          creation: dat.readUnsignedByte()};
-                      return pid;
+            case 103: return new ErlPID(decode(dat),   // Node
+                              dat.readUnsignedInt(),   // ID
+                              dat.readUnsignedInt(),   // Serial
+                              dat.readUnsignedByte()); // Creation
             // Small Tuple
             case 104: var arity = cast(dat.readUnsignedByte(), Int);
                       var tuple = new ErlTuple(arity, true);
@@ -77,14 +70,10 @@ class ETF {
                       var creation = dat.readUnsignedByte();
                       var id = new Array<UInt>();
                       for(i in 0...id_len) id.push(dat.readUnsignedInt());
-                      var ref :ErlRef = {
-                          node:     node,
-                          creation: creation,
-                          id:       id};
-                      return ref;
+                      return new ErlRef(node, id, creation);
             // Small Atom
             case 115: var len = dat.readUnsignedByte();
-                      return {atom: dat.readUTFBytes(len)};
+                      return new ErlAtom(dat.readUTFBytes(len));
             // Function
             // TODO: The num_free cast here for haXe may break big lists!
             case 112: dat.readUnsignedInt(); // Size, which we don't need
@@ -99,38 +88,26 @@ class ETF {
                       var pid = decode(dat);
                       var free_vars = new Array<Dynamic>();
                       for(i in 0...num_free) free_vars.push(decode(dat));
-                      var fun :ErlFun = {
-                          arity:     arity,
-                          uniq:      uniq,
-                          index:     index,
-                          module:    module,
-                          old_index: old_index,
-                          old_uniq:  old_uniq,
-                          pid:       pid,
-                          free_vars: free_vars};
-                      return fun;
+                      return new ErlFun(arity, uniq, index, module, old_index,
+                              old_uniq, pid, free_vars);
             // Export
-            case 113: var export :ErlExport = {
-                          module: decode(dat),
-                          fun:    decode(dat),
-                          arity:  decode(dat)};
-                      return export;
+            case 113: return new ErlExport(
+                              decode(dat),   // Module
+                              decode(dat),   // Fun (atom)
+                              decode(dat));  // Arity
             // Bit Binary
             case 77:  var len = dat.readUnsignedInt();
                       var last_bits = dat.readUnsignedByte();
                       var data = new ByteArray();
                       dat.readBytes(data, 0, len);
-                      var bits :ErlBits = {
-                          data: dat,
-                          last_bits: last_bits};
-                      return bits;
+                      return new ErlBits(dat, last_bits);
             // Float (new)
             case 70:  return dat.readDouble();
 
             // Dunno...
             default:
                       trace("Unknown data!!- what to do about [" +
-                              identifier_byte);
+                              identifier_byte + "]");
                       return null;
         }
     }
@@ -146,40 +123,97 @@ class ETF {
 typedef ErlTuple = flash.Vector<Dynamic>;
 typedef ErlList  = flash.Vector<Dynamic>;
 
-typedef ErlAtom = {
-    var atom      :String;}
+class ErlAtom {
+    public var atom      :String;
+    public function new(atom) {this.atom = atom;}
+    public function toString() {return ':'+atom+':';}
+}
 
-typedef ErlRef = {
-    var node      :ErlAtom;
-    var id        :Array<UInt>;
-    var creation  :UInt;}
+class ErlRef {
+    public var node      :ErlAtom;
+    public var id        :Array<UInt>;
+    public var creation  :UInt;
 
-typedef ErlPort = {
-    var node      :ErlAtom;
-    var id        :UInt;
-    var creation  :UInt;}
+    public function new(node, id, creation) {
+        this.node = node;
+        this.id = id;
+        this.creation = creation;
+    }
+    public function toString() {return 'ErlRef('+node+'|'+id+'|'+creation+')';}
+}
 
-typedef ErlPID = {
-    var node      :ErlAtom;
-    var id        :UInt;
-    var serial    :UInt;
-    var creation  :UInt;}
+class ErlPort {
+    public var node      :ErlAtom;
+    public var id        :Array<UInt>;
+    public var creation  :UInt;
 
-typedef ErlFun = {
-    var arity     :UInt;
-    var uniq      :ByteArray;
-    var index     :UInt;
-    var module    :ErlAtom;
-    var old_index :UInt;
-    var old_uniq  :UInt;
-    var pid       :ErlPID;
-    var free_vars :Array<Dynamic>;}
+    public function new(node, id, creation) {
+        this.node = node;
+        this.id = id;
+        this.creation = creation;
+    }
+    public function toString() {return 'ErlPort('+node+'|'+id+'|'+creation+')';}
+}
 
-typedef ErlExport = {
-    var module    :ErlAtom;
-    var fun       :ErlAtom;
-    var arity     :UInt;}
+class ErlPID {
+    public var node      :ErlAtom;
+    public var id        :UInt;
+    public var serial    :UInt;
+    public var creation  :UInt;
 
-typedef ErlBits = {
-    var data      :ByteArray;
-    var last_bits :UInt;}
+    public function new(node, id, serial, creation) {
+        this.node = node;
+        this.id = id;
+        this.serial = serial;
+        this.creation = creation;
+    }
+
+    public function toString() {
+        return 'ErlPID('+node+'|'+id+'|'+serial+'|'+creation+')';
+    }
+}
+
+class ErlFun {
+    public var arity     :UInt;
+    public var uniq      :ByteArray;
+    public var index     :UInt;
+    public var module    :ErlAtom;
+    public var old_index :UInt;
+    public var old_uniq  :UInt;
+    public var pid       :ErlPID;
+    public var free_vars :Array<Dynamic>;
+
+    public function new(a, u, i, m, oi, ou, p, f) {
+        arity = a; uniq = u; index = i; module = m;
+        old_index = oi; old_uniq = ou; pid = p; free_vars = f;
+    }
+
+    public function toString() {
+        return 'ErlFun('+arity+'|'+index+'|'+module+'|'+free_vars+')';
+    }
+}
+
+
+class ErlExport {
+    public var module    :ErlAtom;
+    public var fun       :ErlAtom;
+    public var arity     :UInt;
+    public function new(m,f,a) {
+        module = m; fun = f; a = arity;
+    }
+    public function toString() {
+        return 'ErlExport('+module+'.'+fun+'/'+arity+')';
+    }
+}
+
+
+class ErlBits {
+    public var data      :ByteArray;
+    public var last_bits :UInt;
+    public function new(d, l) {
+        data = d; last_bits = l;
+    }
+    public function toString() {
+        return 'ErlBits('+data.size()+'|'+last_bits+')';
+    }
+}
