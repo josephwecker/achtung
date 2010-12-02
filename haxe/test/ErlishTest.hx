1b8e1026d5afbe62fe77a7a65b478cdecd842dad
Joseph Wecker <jwecker@justin.tv>
2010-12-01 19:13:45 -0800

    Updated the testing stuff in order to try and get some decoding working.

--- haxe/test/ErlishTest.hx
+++ haxe/test/ErlishTest.hx
@@ -1,6 +1,8 @@
-import utest.Assert;
+/*import utest.Assert;
 
 class ErlishTest {
+    var server :ErlangBridge;
+
     static public function main() {
         utils.Tracer.initialize();
         utils.Tester.run(new ErlishTest());
@@ -8,7 +10,30 @@ class ErlishTest {
 
     public function new();
 
+    public function setup() {
+        server = new ErlangBridge('localhost', 4114);
+    }
+
     public function testJSON() {
         Assert.equals(1, 1, "Oops, those are different.\n.\n.\n.\n.");
+    }
+
+    public function testDataTypes() {
+        server.quicktest();
+    }
+}*/
+
+class ErlishTest extends flash.display.Sprite {
+    var server :ErlangBridge;
+    static public function main() {
+        utils.Tracer.initialize();
+        var main = new ErlishTest();
+    }
+
+    public function new() {
+        super();
+        flash.Lib.current.addChild(this);
+        server = new ErlangBridge('localhost', 4114);
+        server.quicktest();
     }
 }
