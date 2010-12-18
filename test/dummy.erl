457af04e7d397b13b0e8f4dbc91c7dc85d7a7729
Joseph Wecker <jwecker@Joseph-macbook.local>
2010-12-17 18:35:23 -0800

    Starting to think through the structure I really want here in the end.

--- test/dummy.erl
+++ test/dummy.erl
@@ -19,4 +19,10 @@
 
 %another(A,B,C,{D,E}) when is_binary(B) -> [D,E,B,C,A].
 
-my_function([_, this_pattern, V] = H, [_, other_pattern, W]) when V > W -> ok.
+%my_function([_, this_pattern, V] = H, [_, other_pattern, W]) when V > W -> ok.
+my_function(_,_) ->
+  12390,
+  $c,
+  'asdf%f4289##',
+  "Hi \x{2504}",
+  ok.
