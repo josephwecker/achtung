1e8f359eda348645cad54c92810d56f9d8f2db54
Joseph Wecker <jwecker@Joseph-macbook.local>
2011-01-13 10:16:16 -0800

    NOT WORKING.  In the middle of a quick refactor of inlining

--- stdlib/ungpeg/src/ungpeg.erl
+++ stdlib/ungpeg/src/ungpeg.erl
@@ -10,16 +10,20 @@ optimize([]) ->
   error_logger:error_msg("Empty grammar- parse it yourself.",[]);
 optimize([{rule,First,_,_}|_] = AST) ->
   % 1. Turn it into a dictionary
-  {Defs, EntryPoints} = make_defs(AST, dict:new(), [First]),
+  {Defs, Entrances} = make_defs(AST, dict:new(), [First]),
   % 2. Parse the parse-transformation functions & tag parts
   Defs2 = Defs:map(fun process_transformers/2),
   %Defs2 = Defs,
   % 3. Scan for recursive rules & entry-points
-  TopLevels = scan_tls(EntryPoints, Defs2),
+  %TopLevels = scan_tls(EntryPoints, Defs2),
+
   % 4. Warn about any unused productions
 
   % 5. Inline everything that can be, unalias, and minor transforms
-  Defs3 = tr_inline_all(TopLevels:to_list(), TopLevels, Defs2, dict:new()),
+  %Defs3 = tr_inline_all(TopLevels:to_list(), TopLevels, Defs2, dict:new()),
+  {_,{Defs3,TopLevels,_}} =
+  lists:mapfoldl(fun tr_inline/2,
+    {Defs2,dict:from_list([{E,true}||E<-Entrances]),Entrances},Entrances),
   % 6. Combine character classes wherever appropriate
   Defs4 = ast_map(Defs3, fun tr_combine_char/1),
   % 7. 
@@ -55,21 +59,21 @@ expr_mapfold(Fun, Acc, Expr) ->
 %--------------------------------------- SETUP -------------------------------
 %% Create general purpose lookup dictionary for the rules
 make_defs([],Defs,EPs) -> {Defs,EPs};
-make_defs([{rule,Name,Attrs,Expr}|R],Defs,EPs) ->
+make_defs([{rule,Name,Attrs,{ExType,ExAttr,ExBody}}|R],Defs,EPs) ->
   EPs2 = case lists:member(entry,Attrs) of
     true -> [Name|EPs];
     false -> EPs
   end,
-  make_defs(R,Defs:store(Name,{Attrs,Expr}),EPs2).
+  make_defs(R,Defs:store(Name,{ExType,ExAttr++Attrs,ExBody}),EPs2).
 
 % Make sure all tags are where they need to be etc. so that elements can be
 % found again after transformations occur.
 % TODO:
 %   - Deep search so you can tag stuff further down in the rules
 %   - Warn when capturing things not normally captured (literals, notp, etc.)
-process_transformers(Name,{Attrs,Expr}) ->
-  case proplists:lookup(trans,Attrs) of
-    none -> {Attrs,Expr};
+process_transformers(Name,{_ExType,ExAttr,_ExBody}=Expr) ->
+  case proplists:lookup(trans,ExAttr) of
+    none -> Expr;
     {_,TransExpr} ->
       Captures = erl_syntax_lib:fold(
         fun
@@ -85,7 +89,7 @@ process_transformers(Name,{Attrs,Expr}) ->
             end;
           (_,Acc) -> Acc
         end, [], erl_syntax:abstract(TransExpr)),
-      {Attrs, verify_and_tag(Captures, Expr, Name)}
+      verify_and_tag(Captures, Expr, Name)
   end.
 
 %% Verify that those captured variables exist and tag the appropriate
@@ -132,32 +136,46 @@ matches_capture(Tag,{_,Attr,_}) ->
 % Add entrypoints to toplevel and scan rules for recursion (which implies they
 % need to be entrypoints as well).
 
-scan_tls(EPs,Defs) -> scan_tls(EPs,Defs,dict:new()).
-scan_tls([],_Defs,Tops) -> Tops;
-scan_tls([EP|R],Defs,Tops) ->
-  {_Attrs,Body} = Defs:fetch(EP),
-  Tops2 = Tops:store(EP,true),
-  Tops3 = scan_for_tops(Defs,Body,[],Tops2),
-  scan_tls(R,Defs,Tops3).
 
-scan_for_tops(Defs,{call,_,Name},Seen,Tops) ->
-  case dict:is_key(Name,Tops) of
-    true -> Tops;
-    false -> case lists:member(Name,Seen) of
-        true -> Tops:store(Name,true);
-        false ->
-          case Defs:find(Name) of
-            error -> error_logger:error_msg("Couldn't find a definition for ~p",[Name]);
-            {ok, {_,Body}} -> scan_for_tops(Defs,Body,[Name|Seen],Tops)
-          end
-      end
-  end;
-scan_for_tops(Defs,{_,_,Exprs},Seen,Tops) when is_list(Exprs) ->
-  lists:foldl(fun(Body,OldTops) -> scan_for_tops(Defs,Body,Seen,OldTops) end,
-    Tops, Exprs);
-scan_for_tops(_,_,_,Tops) -> Tops.
+
+%scan_tls(EPs,Defs) -> scan_tls(EPs,Defs,dict:new()).
+%scan_tls([],_Defs,Tops) -> Tops;
+%scan_tls([EP|R],Defs,Tops) ->
+%  {_Attrs,Body} = Defs:fetch(EP),
+%  Tops2 = Tops:store(EP,true),
+%  Tops3 = scan_for_tops(Defs,Body,[],Tops2),
+%  scan_tls(R,Defs,Tops3).
+
+%scan_for_tops(Defs,{call,_,Name},Seen,Tops) ->
+%  case dict:is_key(Name,Tops) of
+%    true -> Tops;
+%    false -> case lists:member(Name,Seen) of
+%        true -> Tops:store(Name,true);
+%        false ->
+%          case Defs:find(Name) of
+%            error -> error_logger:error_msg("Couldn't find a definition for ~p",[Name]);
+%            {ok, {_,Body}} -> scan_for_tops(Defs,Body,[Name|Seen],Tops)
+%          end
+%      end
+%  end;
+%scan_for_tops(Defs,{_,_,Exprs},Seen,Tops) when is_list(Exprs) ->
+%  lists:foldl(fun(Body,OldTops) -> scan_for_tops(Defs,Body,Seen,OldTops) end,
+%    Tops, Exprs);
+%scan_for_tops(_,_,_,Tops) -> Tops.
 
 %--------------------------------------- TRANSFORMATIONS ---------------------
+
+tr_inline(Entrance, {Defs, TLs, Seen}) ->
+  % TODO: here combine scan_tls (above) and tr_inline_all (below)
+  %   But separate the lit -> char to its own transformation
+  {ok, expr_mapfold(fun tr_inline_inner/2,
+      {Defs,TLs,Seen},Defs:fetch(Entrance))}.
+
+tr_inline_inner({call, CallAttr, Name}=InExpr, {Defs, TLs, Seen}) ->
+  case TLs:is_key(Name) of
+    true -> InExpr;
+    false -> case lists:member(Name,Seen) of
+        true -> 
 
 % - TODO: Tag all parts first for transformations
 % Inlining performs the following transformations explicitly or implicitly:
