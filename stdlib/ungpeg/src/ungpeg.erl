c0d9e83ada6aaa968a08e5e1c3c5d88ee33f5916
Joseph Wecker <jwecker@173-150-197-152.pools.spcsdns.net>
2011-01-12 18:44:18 -0800

    Mostly reorganized

--- stdlib/ungpeg/src/ungpeg.erl
+++ stdlib/ungpeg/src/ungpeg.erl
@@ -2,6 +2,7 @@
 -export([file/1, parse/1, optimize/1]).
 
 
+%--------------------------------------- TOPLEVEL ----------------------------
 file(FName) -> optimize(ungpeg_n:file(FName)).
 parse(Txt) when is_list(Txt) -> optimize(ungpeg_n:parse(Txt)).
 
@@ -27,8 +28,32 @@ optimize([{rule,First,_,_}|_] = AST) ->
 
 optimize(AST) -> error_logger:error_msg("AST doesn't look well:~n  ~p~n~n",[AST]).
 
-% Create general purpose lookup dictionary for the rules
+% Some general utilities for mapping and folding the ast/expression
+ast_map(Defs,Fun) ->
+  Defs:map(fun(_Name,Expr)-> expr_map(Expr, Fun) end).
 
+expr_map(Expr, Fun) ->
+  Expr2 = Fun(Expr),
+  case Expr2 of
+    {ord,Attr,L} -> {ord,Attr,[expr_map(L2,Fun)||L2<-L]};
+    {seq,Attr,L} -> {seq,Attr,[expr_map(L2,Fun)||L2<-L]};
+    _ -> Expr2
+  end.
+
+expr_mapfold(Fun, Acc, Expr) ->
+  {Expr2, Acc2} = Fun(Expr, Acc),
+  case Expr2 of
+    {ord,Attr,L} ->
+      {Expr3,Acc3}=lists:mapfoldl(fun(LExp,AccIn)->expr_mapfold(Fun,AccIn,LExp) end,Acc2,L),
+      {{ord,Attr,Expr3}, Acc3};
+    {seq,Attr,L} ->
+      {Expr3,Acc3}=lists:mapfoldl(fun(LExp,AccIn)->expr_mapfold(Fun,AccIn,LExp) end,Acc2,L),
+      {{seq,Attr,Expr3}, Acc3};
+    _ -> {Expr2, Acc2}
+  end.
+
+%--------------------------------------- SETUP -------------------------------
+%% Create general purpose lookup dictionary for the rules
 make_defs([],Defs,EPs) -> {Defs,EPs};
 make_defs([{rule,Name,Attrs,Expr}|R],Defs,EPs) ->
   EPs2 = case lists:member(entry,Attrs) of
@@ -37,12 +62,11 @@ make_defs([{rule,Name,Attrs,Expr}|R],Defs,EPs) ->
   end,
   make_defs(R,Defs:store(Name,{Attrs,Expr}),EPs2).
 
-
 % Make sure all tags are where they need to be etc. so that elements can be
 % found again after transformations occur.
 % TODO:
 %   - Deep search so you can tag stuff further down in the rules
-%   - Warn when capturing things not normally captured (literals, !e, etc.)
+%   - Warn when capturing things not normally captured (literals, notp, etc.)
 process_transformers(Name,{Attrs,Expr}) ->
   case proplists:lookup(trans,Attrs) of
     none -> {Attrs,Expr};
@@ -64,26 +88,23 @@ process_transformers(Name,{Attrs,Expr}) ->
       {Attrs, verify_and_tag(Captures, Expr, Name)}
   end.
 
+%% Verify that those captured variables exist and tag the appropriate
+%% expressions in case things get reorganized.
 verify_and_tag([],Expr,_Name) -> Expr;
-
-% Location/position-based captures
+% $n when there aren't that many parts
 verify_and_tag([N|_],{_,_,[_|_]=L},Name) when is_number(N) and (N > length(L)) ->
   error_logger:error_msg("There is nothing at position $~p for rule ~p",[N,Name]);
-
-verify_and_tag([0|R],Expr,Name) ->
-  verify_and_tag(R,tag_expr(Expr,0,Name),Name);
-
+% $0 or $_ --> whole thing
+verify_and_tag([N|R],Expr,Name) when (N==0) or (N=='_')->
+  verify_and_tag(R,tag_expr(Expr,N,Name),Name);
+% Position based capture
 verify_and_tag([N|R],{Type,Attr,[_|_]=L},Name) when is_number(N) and (N =< length(L)) ->
   {Before, [Change|After]} = lists:split(N-1,L),
   TaggedExp = tag_expr(Change,N,Name),
   verify_and_tag(R,{Type,Attr,Before++[TaggedExp]++After},Name);
-
+% When $1 acts like $_ because there's only one part
 verify_and_tag([1|R],Expr,Name) ->
   verify_and_tag(R,tag_expr(Expr,1,Name),Name);
-
-verify_and_tag(['_'|R],Expr,Name) ->
-  verify_and_tag(R,tag_expr(Expr,'_',Name),Name);
-
 % Named captures
 verify_and_tag([A|R],Expr,Name) when is_atom(A) ->
   Res = expr_mapfold(fun(E,Acc) ->
@@ -110,6 +131,7 @@ matches_capture(Tag,{_,Attr,_}) ->
 
 % Add entrypoints to toplevel and scan rules for recursion (which implies they
 % need to be entrypoints as well).
+
 scan_tls(EPs,Defs) -> scan_tls(EPs,Defs,dict:new()).
 scan_tls([],_Defs,Tops) -> Tops;
 scan_tls([EP|R],Defs,Tops) ->
@@ -134,28 +156,6 @@ scan_for_tops(Defs,{_,_,Exprs},Seen,Tops) when is_list(Exprs) ->
   lists:foldl(fun(Body,OldTops) -> scan_for_tops(Defs,Body,Seen,OldTops) end,
     Tops, Exprs);
 scan_for_tops(_,_,_,Tops) -> Tops.
-
-ast_map(Defs,Fun) ->
-  Defs:map(fun(_Name,Expr)-> ast_map_inner(Expr, Fun) end).
-ast_map_inner(Expr, Fun) ->
-  Expr2 = Fun(Expr),
-  case Expr2 of
-    {ord,Attr,L} -> {ord,Attr,[ast_map_inner(L2,Fun)||L2<-L]};
-    {seq,Attr,L} -> {seq,Attr,[ast_map_inner(L2,Fun)||L2<-L]};
-    _ -> Expr2
-  end.
-
-expr_mapfold(Fun, Acc, Expr) ->
-  {Expr2, Acc2} = Fun(Expr, Acc),
-  case Expr2 of
-    {ord,Attr,L} ->
-      {Expr3,Acc3}=lists:mapfoldl(fun(LExp,AccIn)->expr_mapfold(Fun,AccIn,LExp) end,Acc2,L),
-      {{ord,Attr,Expr3}, Acc3};
-    {seq,Attr,L} ->
-      {Expr3,Acc3}=lists:mapfoldl(fun(LExp,AccIn)->expr_mapfold(Fun,AccIn,LExp) end,Acc2,L),
-      {{seq,Attr,Expr3}, Acc3};
-    _ -> {Expr2, Acc2}
-  end.
 
 %--------------------------------------- TRANSFORMATIONS ---------------------
 
