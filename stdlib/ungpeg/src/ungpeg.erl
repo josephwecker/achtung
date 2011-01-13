8e52910f7962adf753e079c30a6ce7282e91dde1
Joseph Wecker <jwecker@justin.tv>
2011-01-13 12:03:20 -0800

    Refactored a bit (way bigger pain than I expected)- simpler and more maintainable hopefully.

--- stdlib/ungpeg/src/ungpeg.erl
+++ stdlib/ungpeg/src/ungpeg.erl
@@ -13,22 +13,20 @@ optimize([{rule,First,_,_}|_] = AST) ->
   {Defs, Entrances} = make_defs(AST, dict:new(), [First]),
   % 2. Parse the parse-transformation functions & tag parts
   Defs2 = Defs:map(fun process_transformers/2),
-  %Defs2 = Defs,
   % 3. Scan for recursive rules & entry-points
-  %TopLevels = scan_tls(EntryPoints, Defs2),
-
+  TopLevelNames = get_toplevels(Entrances, Defs2),
   % 4. Warn about any unused productions
 
-  % 5. Inline everything that can be, unalias, and minor transforms
-  %Defs3 = tr_inline_all(TopLevels:to_list(), TopLevels, Defs2, dict:new()),
-  {_,{Defs3,TopLevels,_}} =
-  lists:mapfoldl(fun tr_inline/2,
-    {Defs2,dict:from_list([{E,true}||E<-Entrances]),Entrances},Entrances),
-  % 6. Combine character classes wherever appropriate
-  Defs4 = ast_map(Defs3, fun tr_combine_char/1),
-  % 7. 
+  % 5. Transform: Inline everything that can be (unalias)
+  MainExprs = TopLevelNames:map(
+    fun(TName,true)-> expr_map(Defs2:fetch(TName),
+          fun(ExprIn)->tr_inline(ExprIn,TopLevelNames,Defs2) end) end),
+  % 6. Tranform: Simple literals into character-classes
+  Main2 = ast_map(MainExprs, fun tr_simple_lit_to_char/1),
+  % 7. Combine character classes wherever appropriate
+  Main3 = ast_map(Main2, fun tr_combine_char/1),
 
-  Defs4:to_list();
+  Main3:to_list();
 
 optimize(AST) -> error_logger:error_msg("AST doesn't look well:~n  ~p~n~n",[AST]).
 
@@ -134,80 +132,51 @@ matches_capture(Tag,{_,Attr,_}) ->
 
 
 % Add entrypoints to toplevel and scan rules for recursion (which implies they
-% need to be entrypoints as well).
+% need to be entrypoints as well).  Don't do multis yet- they'll be separated
+% out later after some transformations.
+get_toplevels(EPs,Defs) -> get_toplevels(EPs,Defs,dict:new()).
+get_toplevels([],_Defs,Tops) -> Tops;
+get_toplevels([EP|R],Defs,Tops) ->
+  Body = Defs:fetch(EP),
+  Tops2 = Tops:store(EP,true),
+  Tops3 = get_tl_inner(Defs,Body,[EP],Tops2),
+  get_toplevels(R,Defs,Tops3).
 
-
-
-%scan_tls(EPs,Defs) -> scan_tls(EPs,Defs,dict:new()).
-%scan_tls([],_Defs,Tops) -> Tops;
-%scan_tls([EP|R],Defs,Tops) ->
-%  {_Attrs,Body} = Defs:fetch(EP),
-%  Tops2 = Tops:store(EP,true),
-%  Tops3 = scan_for_tops(Defs,Body,[],Tops2),
-%  scan_tls(R,Defs,Tops3).
-
-%scan_for_tops(Defs,{call,_,Name},Seen,Tops) ->
-%  case dict:is_key(Name,Tops) of
-%    true -> Tops;
-%    false -> case lists:member(Name,Seen) of
-%        true -> Tops:store(Name,true);
-%        false ->
-%          case Defs:find(Name) of
-%            error -> error_logger:error_msg("Couldn't find a definition for ~p",[Name]);
-%            {ok, {_,Body}} -> scan_for_tops(Defs,Body,[Name|Seen],Tops)
-%          end
-%      end
-%  end;
-%scan_for_tops(Defs,{_,_,Exprs},Seen,Tops) when is_list(Exprs) ->
-%  lists:foldl(fun(Body,OldTops) -> scan_for_tops(Defs,Body,Seen,OldTops) end,
-%    Tops, Exprs);
-%scan_for_tops(_,_,_,Tops) -> Tops.
+get_tl_inner(Defs,{call,_,Name},Seen,Tops) ->
+  case Tops:is_key(Name) of
+    true -> Tops;
+    false -> case lists:member(Name,Seen) of
+        true -> Tops:store(Name,true);
+        false ->
+          case Defs:find(Name) of
+            error -> error_logger:error_msg("Couldn't find a definition for ~p",[Name]);
+            {ok, Body} -> get_tl_inner(Defs,Body,[Name|Seen],Tops)
+          end
+      end
+  end;
+get_tl_inner(Defs,{_,_,Exprs},Seen,Tops) when is_list(Exprs) ->
+  lists:foldl(fun(Body,OldTops) -> get_tl_inner(Defs,Body,Seen,OldTops) end,
+    Tops, Exprs);
+get_tl_inner(_,_,_,Tops) -> Tops.
 
 %--------------------------------------- TRANSFORMATIONS ---------------------
 
-tr_inline(Entrance, {Defs, TLs, Seen}) ->
-  % TODO: here combine scan_tls (above) and tr_inline_all (below)
-  %   But separate the lit -> char to its own transformation
-  {ok, expr_mapfold(fun tr_inline_inner/2,
-      {Defs,TLs,Seen},Defs:fetch(Entrance))}.
-
-tr_inline_inner({call, CallAttr, Name}=InExpr, {Defs, TLs, Seen}) ->
-  case TLs:is_key(Name) of
-    true -> InExpr;
-    false -> case lists:member(Name,Seen) of
-        true -> 
-
-% - TODO: Tag all parts first for transformations
-% Inlining performs the following transformations explicitly or implicitly:
-% - multi-level unalias
-% - single-literals to charclass
-% - normalize attributes (TODO)
-%   - reduce
-%   - remove duplicates
-%
-tr_inline_all([],_TLevs,_Defs,NewDefs) -> NewDefs;
-tr_inline_all([{EP,true}|R],TLevs,Defs,NewDefs) ->
-  {RuleAttrs,Expr} = Defs:fetch(EP),
-  {InnerType, InnerAttr, InnerBody} = tr_inline_parts(Expr,TLevs,Defs),
-  % Attributes in order- apply from left to right
-  tr_inline_all(R,TLevs,Defs,
-    NewDefs:store(EP,{InnerType,InnerAttr++RuleAttrs,InnerBody})).
-
-tr_inline_parts({lit,Attrs,[Chr]},_TLevs,_Defs) -> {char,Attrs,[Chr]};
-tr_inline_parts({Type,Attrs,Exprs},TLevs,Defs) when is_list(Exprs) ->
-  {Type,Attrs,[tr_inline_parts(E,TLevs,Defs)||E<-Exprs]};
-tr_inline_parts({call,CallAttrs,Name},TLevs,Defs) ->
-  case TLevs:find(Name) of
-    {ok,_} -> {call,CallAttrs,Name};
-    _ ->
-      {RuleAttrs, {ExprType,RuleTopExprAttrs,EBody}} = Defs:fetch(Name),
-      % Attributes in order- apply from left to right
-      tr_inline_parts({ExprType,RuleTopExprAttrs ++ RuleAttrs ++ CallAttrs, EBody},TLevs,Defs)
+% Multi-level unalias
+tr_inline({call,CallAttrs,Name}=Call, TLNames, Defs) ->
+  case TLNames:is_key(Name) of
+    true -> Call;
+    false ->
+      {ExprType, ExprAttrs, ExprBody} = Defs:fetch(Name),
+      tr_inline({ExprType,ExprAttrs++CallAttrs,ExprBody},TLNames,Defs)
   end;
-tr_inline_parts(Atomic,_TLevs,_Defs) -> Atomic.
+tr_inline(Expr, _TLNames, _Defs) -> Expr.
 
+% Turn literals that only have one char into character class (in case it can,
+% in turn, be combined)
+tr_simple_lit_to_char({lit,Attrs,<<Chr>>}) -> {char,Attrs,[Chr]};
+tr_simple_lit_to_char(Other) -> Other.
 
-% - combine ord-charclasses into charclass
+% Combine ord-charclasses into charclass
 tr_combine_char({ord,Attr,OrdList}) -> {ord, Attr, tr_combine_char_inner(OrdList,[])};
 tr_combine_char(Other) -> Other.
 tr_combine_char_inner([],Acc) -> lists:reverse(Acc);
