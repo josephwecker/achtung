3e5394781643204517159ead771e5a39dd29b3b1
Joseph Wecker <jwecker@Joseph-macbook.local>
2011-02-05 23:20:22 -0800

    Continuing to update all the terms

--- stdlib/ungpeg/src/ungpeg_optimize.erl
+++ stdlib/ungpeg/src/ungpeg_optimize.erl
@@ -41,12 +41,12 @@ optimize(AST, _State) -> error_logger:error_msg("AST doesn't look well:~n  ~p~n~
 all_transformations(Defs) ->
   lists:foldl(fun ast_map/2,
     Defs, [
-      %fun tr_simple_lit_to_char/1,
-      %fun tr_combine_char/1
-      fun tr_lits_to_chars/1,
-      fun tr_expand_chars/1,
+      %fun tr_simple_lit_to_chr/1,
+      %fun tr_combine_chr/1
+      fun tr_lits_to_chrs/1,
+      fun tr_expand_chrs/1,
       fun tr_expand_plusses/1    %% Now in 2nd form (assuming inlining has taken place)
-      %fun tr_condense_ords_seqs/1
+      %fun tr_condense_pchs_seqs/1
     ]).
 
 % Some general utilities for mapping and folding the ast/expression.  We
@@ -61,7 +61,7 @@ expr_map(Expr, Fun) ->
   % TODO: flatten and remove []'s so that they can be expanded & shrunk
   Expr2 = Fun(Expr),
   case Expr2 of
-    {ord,Attr,L} -> {ord,Attr,[expr_map(L2,Fun)||L2<-L]};
+    {pch,Attr,L} -> {pch,Attr,[expr_map(L2,Fun)||L2<-L]};
     {seq,Attr,L} -> {seq,Attr,[expr_map(L2,Fun)||L2<-L]};
     _ -> Expr2
   end.
@@ -69,9 +69,9 @@ expr_map(Expr, Fun) ->
 expr_mapfold(Fun, Acc, Expr) ->
   {Expr2, Acc2} = Fun(Expr, Acc),
   case Expr2 of
-    {ord,Attr,L} ->
+    {pch,Attr,L} ->
       {Expr3,Acc3}=lists:mapfoldl(fun(LExp,AccIn)->expr_mapfold(Fun,AccIn,LExp) end,Acc2,L),
-      {{ord,Attr,Expr3}, Acc3};
+      {{pch,Attr,Expr3}, Acc3};
     {seq,Attr,L} ->
       {Expr3,Acc3}=lists:mapfoldl(fun(LExp,AccIn)->expr_mapfold(Fun,AccIn,LExp) end,Acc2,L),
       {{seq,Attr,Expr3}, Acc3};
@@ -148,8 +148,8 @@ verify_and_tag([A|R],Expr,Name) when is_atom(A) ->
 
 tag_expr({Type,Attr,Expr},Tag,Name) -> {Type,[{tag,{Tag,Name}}|Attr],Expr}.
 
-matches_capture(Tag,{call,_,Tag}) -> true;
-matches_capture(Special,{special,_,Special}) -> true;
+matches_capture(Tag,{ref,_,Tag}) -> true;
+%matches_capture(Special,{special,_,Special}) -> true;
 matches_capture(Tag,{_,Attr,_}) ->
   lists:member({orig_tag,Tag},Attr) or
   lists:member({orig,Tag},Attr).
@@ -166,7 +166,7 @@ get_toplevels([EP|R],Defs,Tops) ->
   Tops3 = get_tl_inner(Defs,Body,[EP],Tops2),
   get_toplevels(R,Defs,Tops3).
 
-get_tl_inner(Defs,{call,_,Name},Seen,Tops) ->
+get_tl_inner(Defs,{ref,_,Name},Seen,Tops) ->
   case Tops:is_key(Name) of
     true -> Tops;
     false -> case lists:member(Name,Seen) of
@@ -207,7 +207,7 @@ get_tl_inner(_,_,_,Tops) -> Tops.
 
 %--------------------------------------- TRANSFORMATIONS ---------------------
 % Multi-level unalias
-tr_inline({call,CallAttrs,Name}=Call, TLNames, Defs) ->
+tr_inline({ref,CallAttrs,Name}=Call, TLNames, Defs) ->
   case TLNames:is_key(Name) of
     true -> Call;
     false ->
@@ -220,23 +220,23 @@ tr_inline(Expr, _TLNames, _Defs) -> Expr.
 %% in turn, be combined)
 %% Before:  a <- 'a' 'bc'
 %% After:   a <- [a] 'bc'
-%tr_simple_lit_to_char({lit,Attrs,<<Chr>>}) -> {char,Attrs,[Chr]};
-%tr_simple_lit_to_char(Other) -> Other.
+%tr_simple_lit_to_chr({lit,Attrs,<<Chr>>}) -> {chr,Attrs,[Chr]};
+%tr_simple_lit_to_chr(Other) -> Other.
 
-%% Combine ord-charclasses into charclass. Captures no problem because we're in
-%% an ord, but if any are prefixed or suffixed this transform doesn't apply.
+%% Combine pch-charclasses into charclass. Captures no problem because we're in
+%% an pch, but if any are prefixed or suffixed this transform doesn't apply.
 %% Before:  a <- [a]/[b]/[c]
 %% After:   a <- [abc]
-%tr_combine_char({ord,Attr,OrdList})->{ord,Attr,tr_combine_char_inner(OrdList,[])};
-%tr_combine_char(Other) -> Other.
-%tr_combine_char_inner([],Acc) -> lists:reverse(Acc);
-%tr_combine_char_inner([One],Acc) -> lists:reverse([One|Acc]);
-%tr_combine_char_inner([{char,A1,Ranges1}|[{char,A2,Ranges2}|R]],Acc) ->
+%tr_combine_chr({pch,Attr,PchList})->{pch,Attr,tr_combine_chr_inner(PchList,[])};
+%tr_combine_chr(Other) -> Other.
+%tr_combine_chr_inner([],Acc) -> lists:reverse(Acc);
+%tr_combine_chr_inner([One],Acc) -> lists:reverse([One|Acc]);
+%tr_combine_chr_inner([{chr,A1,Ranges1}|[{chr,A2,Ranges2}|R]],Acc) ->
 %  case {A1,A2} of
 %    {[],[]} -> 
 %  ((A1 == []) or (A1 == [orig])) and ((A2 == []) or (A2 == [orig])) ->
-%    tr_combine_char_inner([{char,A1++A2,lists:usort(Ranges1++Ranges2)}|R], Acc);
-%tr_combine_char_inner([Other|R],Acc) -> tr_combine_char_inner(R,[Other|Acc]).
+%    tr_combine_chr_inner([{chr,A1++A2,lists:usort(Ranges1++Ranges2)}|R], Acc);
+%tr_combine_chr_inner([Other|R],Acc) -> tr_combine_chr_inner(R,[Other|Acc]).
 
 % Sequential literals or 1-char charclasses combine
 % Before:  a <- 'hey' [\t] ':'
@@ -244,13 +244,13 @@ tr_inline(Expr, _TLNames, _Defs) -> Expr.
 
 %tr_combine_lits({seq,Attr,EList})->{seq,Attr,tr_combine_lits_inner(EList,[])};
 
-tr_lits_to_chars({lit,A,<<Char>>}) ->{char,A,[Char]};
-tr_lits_to_chars({lit,A,Chars})->{seq,A,[{char,[],[C]}||C<-binary_to_list(Chars)]};
-tr_lits_to_chars(Other)->Other.
+tr_lits_to_chrs({lit,A,<<Char>>}) ->{chr,A,[Char]};
+tr_lits_to_chrs({lit,A,Chars})->{seq,A,[{chr,[],[C]}||C<-binary_to_list(Chars)]};
+tr_lits_to_chrs(Other)->Other.
 
-tr_expand_chars({char,_,[_]}=E)-> E;
-tr_expand_chars({char,Attrs,Ranges})->{ord,Attrs,[{char,[],[R]}||R<-Ranges]};
-tr_expand_chars(Other)->Other.
+tr_expand_chrs({chr,_,[_]}=E)-> E;
+tr_expand_chrs({chr,Attrs,Ranges})->{pch,Attrs,[{chr,[],[R]}||R<-Ranges]};
+tr_expand_chrs(Other)->Other.
 
 tr_expand_plusses({Type,Attrs,Body}=E)->
   case lists:member(plus,Attrs) of
@@ -269,11 +269,11 @@ tr_expand_plusses({Type,Attrs,Body}=E)->
 %        false -> first_fixed_i(E)
 %      end
 %  end.
-%first_fixed_i({char,_,[R]}) -> R;
-%first_fixed_i({ord,A,Exprs}) ->
+%first_fixed_i({chr,_,[R]}) -> R;
+%first_fixed_i({pch,A,Exprs}) ->
 %  R = common_prefix([first_fixed(E)||E<-Exprs]),
   %io:format("Common prefix for ~p: <~s>~n",
-  %  [{ord,A},lists:map(fun({C1,C2})->[$[,C1,$-,C2,$]];(C3)->C3 end, R)]),
+  %  [{pch,A},lists:map(fun({C1,C2})->[$[,C1,$-,C2,$]];(C3)->C3 end, R)]),
 %  R;
 %first_fixed_i({seq,A,Exprs}) ->
 %  R = common_prefix([lists:flatten([first_fixed(E)||E<-Exprs])]),
@@ -305,30 +305,30 @@ pretty_print([], Acc)  -> io:format("~s~n~n", [string:join(lists:reverse(Acc),"\
 pretty_print([{Name,Expr}|R], Acc) ->
   pretty_print(R, [[atom_to_list(Name), " <- ", pretty_print_expr(Expr,1)] | Acc]).
 
-% ord within seq always parenth.
-% ord within ord always parenth.
+% pch within seq always parenth.
+% pch within pch always parenth.
 % seq within seq always parenth.
 % space if all children !atomic
 
 pretty_print_expr(Expr)     -> pretty_print_expr(Expr,1,none).
 pretty_print_expr(Expr,Lvl) -> pretty_print_expr(Expr,Lvl,none).
 
-pretty_print_expr({char,Attr,Ranges},_Lvl,_Parent) ->
+pretty_print_expr({chr,Attr,Ranges},_Lvl,_Parent) ->
   [pp_prefs(Attr),"[",lists:map(fun({C1,C2})->esc_str([C1,$-,C2]);(Ch)->esc_str(Ch) end,Ranges),"]",pp_suffs(Attr)];
 pretty_print_expr({lit,Attr,Bin},_Lvl,_Parent) ->
   [pp_prefs(Attr),"'",chr_replace($',"\\'",esc_str(binary_to_list(Bin))),"'",pp_suffs(Attr)];
-pretty_print_expr({call,Attr,Name},_Lvl,_Parent) ->
+pretty_print_expr({ref,Attr,Name},_Lvl,_Parent) ->
   [pp_prefs(Attr),atom_to_list(Name),pp_suffs(Attr)];
-pretty_print_expr({special,Attr,Type},_Lvl,_Parent) ->
-  [pp_prefs(Attr),atom_to_list(Type),pp_suffs(Attr)];
-pretty_print_expr({ord,Attr,Exprs},Lvl,Parent) ->
+pretty_print_expr({S,Attr,_},_Lvl,_Parent) when S==fin;S==eof;S==eps;S==fail ->
+  [pp_prefs(Attr),atom_to_list(S),pp_suffs(Attr)];
+pretty_print_expr({pch,Attr,Exprs},Lvl,Parent) ->
   Prefs = pp_prefs(Attr),
   Suffs = pp_suffs(Attr),
-  Paren  = (length(Prefs++Suffs) > 0) or (Parent == seq) or (Parent == ord),
+  Paren  = (length(Prefs++Suffs) > 0) or (Parent == seq) or (Parent == pch),
   {Begin,End} = case Paren of false->{[],[]};_->{[Prefs,$(],[$),Suffs]} end,
   case all_atomics(Exprs) of
-    true -> [Begin,string:join([pretty_print_expr(E,Lvl+1,ord)||E<-Exprs],"/"),End];
-    false-> [Begin,string:join([pretty_print_expr(E,Lvl+1,ord)||E<-Exprs]," / "),End]
+    true -> [Begin,string:join([pretty_print_expr(E,Lvl+1,pch)||E<-Exprs],"/"),End];
+    false-> [Begin,string:join([pretty_print_expr(E,Lvl+1,pch)||E<-Exprs]," / "),End]
   end;
 pretty_print_expr({seq,Attr,Exprs},Lvl,Parent) ->
   Prefs = pp_prefs(Attr),
@@ -352,12 +352,14 @@ pp_suffs([_|R],Acc)            -> pp_suffs(R,Acc).
 
 all_atomics(L) ->
   lists:all(fun
-      ({char,_,_})    -> true;
-      ({lit,_,_})     -> true;
-      ({special,_,_}) -> true;
-      ({call,_,_})    -> true;
-      ({ord,_,_})     -> false;
-      ({seq,_,_})     -> false
+      ({chr,_,_}) -> true;
+      ({lit,_,_}) -> true;
+      ({eof,_,_}) -> true;
+      ({fin,_,_}) -> true;
+      ({eps,_,_}) -> true;
+      ({ref,_,_}) -> true;
+      ({pch,_,_}) -> false;
+      ({seq,_,_}) -> false
     end, L).
 
 chr_replace(Needle,Replacement,Haystack) ->
